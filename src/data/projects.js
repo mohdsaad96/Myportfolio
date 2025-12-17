7bc7373d33ae6d53153bf48ee56eaf303895fe01
mohdsaad96 <msaadm96@gmail.com>
2025-12-18 01:19:54 +0530

    update: project data and add workflow config

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,34 +6,39 @@ const projects = [
     tech: ['SketchUp','Enscape','AutoCAD'],
     live: '#',
     code: '#',
-    image: '/src/assets/project-placeholder.png' 
+    image: ['/src/assets/enscape project3.png',
+            '/src/assets/enscape project3 (2).png'],
   },
   {
     id: 2,
     title: 'Construction Drawing Set',
-    description: 'Detailed 2D construction drawings and RCC design for a G+1 residential building (AutoCAD).',
-    tech: ['AutoCAD','RC Design'],
+    description: 'Detailed 2D & 3D construction drawings and design for a G+1 residential building (AutoCAD).',
+    tech: ['AutoCAD','Vray','SketchUp'],
     live: '#',
     code: '#',
-    image: '/src/assets/project-placeholder.png'
+    image: ['/src/assets/enscape project2 (2).png',
+           '/src/assets/enscape project2 (3).png'],
   },
   {
     id: 3,
     title: 'Residential 3D Modeling — Deole sir house',
-    description: 'Complete interior and exterior modeling for a 20\'x50\' house using SketchUp and Enscape for client presentation renders.',
+    description: 'Complete interior and exterior modeling for a 40\'x35\' house using SketchUp and Enscape for client presentation renders.',
     tech: ['SketchUp','Enscape','AutoCAD'],
     live: '#',
     code: '#',
-    image: '/src/assets/enscape project3.png' 
+    image: ['/src/assets/enscape project4 (2).png',
+           '/src/assets/enscape project4(3).png'],
   },
   {
     id: 4,
     title: 'Residential 3D Modeling — Project 1',
-    description: 'Complete interior and exterior modeling for a 20\'x50\' house using SketchUp and Enscape for client presentation renders.',
+    description: 'Complete interior and exterior modeling for a 35\'x50\' house using SketchUp and Enscape for client presentation renders.',
     tech: ['SketchUp','Enscape','AutoCAD'],
     live: '#',
     code: '#',
-    image: '/src/assets/enscape project 1.png' 
+    image: ['/src/assets/enscape project 1.png',
+          '/src/assets/enscape project 1 (3).png',
+         '/src/assets/enscape project 1 (2).png']
   },
 ];
 
